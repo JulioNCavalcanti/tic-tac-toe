43bb4155003550e900cea526479b6a658a641ed6
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-18 18:14:55 -0300

    Including the function to print the thank you message

---
+++ print_thank_you_message.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "print_header.h"
+
+void print_thank_you_message()
+{
+    print_header();
+    printf("|                                                                          |\n");
+    printf("|            --------------------------------------------------            |\n");
+    printf("|           |     Thank you very much for playing my game!     |           |\n");
+    printf("|           |                See you next time.                |           |\n");
+    printf("|            --------------------------------------------------            |\n");
+    printf("|                                                                          |\n");
+    printf(" --------------------------------------------------------------------------  \n");
+}
