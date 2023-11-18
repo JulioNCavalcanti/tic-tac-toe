1b8093f31a9051d7ce5dbe1339d461e04fa76b43
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-18 11:45:57 -0300

    Refactoring the function

--- check_the_winner.c
+++ check_the_winner.c
@@ -5,16 +5,16 @@ int check_the_winner(char player)
     char(*board)[76] = get_board();
 
     if ( // Line
-        (board[6][33] == player && board[6][37] == player && board[6][41] == player) ||
-        (board[8][33] == player && board[8][37] == player && board[8][41] == player) ||
-        (board[10][33] == player && board[10][37] == player && board[10][41] == player) ||
+        (board[7][33] == player && board[7][37] == player && board[7][41] == player) ||
+        (board[9][33] == player && board[9][37] == player && board[9][41] == player) ||
+        (board[11][33] == player && board[11][37] == player && board[11][41] == player) ||
         // Column
-        (board[6][33] == player && board[8][33] == player && board[10][33] == player) ||
-        (board[6][37] == player && board[8][37] == player && board[10][37] == player) ||
-        (board[6][41] == player && board[8][41] == player && board[10][41] == player) ||
+        (board[7][33] == player && board[9][33] == player && board[11][33] == player) ||
+        (board[7][37] == player && board[9][37] == player && board[11][37] == player) ||
+        (board[7][41] == player && board[9][41] == player && board[11][41] == player) ||
         // Diagonal
-        (board[6][33] == player && board[8][37] == player && board[10][41] == player) ||
-        (board[10][33] == player && board[8][37] == player && board[6][41] == player))
+        (board[7][33] == player && board[9][37] == player && board[11][41] == player) ||
+        (board[11][33] == player && board[9][37] == player && board[7][41] == player))
     {
         return 1;
     }
