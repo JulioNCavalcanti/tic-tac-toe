abe2067fd75dc229f946d7a85cba67e70a3637aa
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 09:24:49 -0300

    Adding colors

--- print_about.c
+++ print_about.c
@@ -2,20 +2,21 @@
 #include "print_header.h"
 #include "return_to_menu.h"
 #include "print_menu.h"
+#include "colors.h"
 
 void print_about()
 {
     print_header();
-    
+
     printf("|                                                                          |\n");
     printf("|                             -----------------                            |\n");
-    printf("|                            |     CREDITS     |                           |\n");
+    printf("|                            |     %sCREDITS%s     |                           |\n", BLUE, RESET);
     printf("|                             -----------------                            |\n");
     printf("|                                                                          |\n");
-    printf("| Development:                                                             |\n");
+    printf("| %sDevelopment:%s                                                             |\n", PURPLE, RESET);
     printf("|     - Julio Cavalcanti                                                   |\n");
     printf("|                                                                          |\n");
-    printf("| GitHub:                                                                  |\n");
+    printf("| %sGitHub:%s                                                                  |\n", PURPLE, RESET);
     printf("|     - https://github.com/JulioNCavalcanti                                |\n");
     printf("|                                                                          |\n");
     printf(" -------------------------------------------------------------------------- \n");
