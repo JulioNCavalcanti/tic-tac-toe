f6de8c7b74163435f651aa923b260084e19fdbba
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 06:04:39 -0300

    Including the continue game function

---
+++ continue_the_game.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "clear_buffer.h"
+
+void continue_the_game()
+{
+    printf("\n");
+    printf("Press Enter to continue the game... ");
+    clear_buffer();
+}
---
+++ continue_the_game.h
@@ -0,0 +1,6 @@
+#ifndef CONTINUE_THE_GAME_H
+#define CONTINUE_THE_GAME_H
+
+void continue_the_game();
+
+#endif
