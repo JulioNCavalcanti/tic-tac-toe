69c8fc4282113f67a9ae2b34798a750127bddec1
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 18:24:28 -0300

    including header file to define colors

---
+++ colors_definitions.h
@@ -0,0 +1,11 @@
+#ifndef COLORS_DEFINITIONS_H
+#define COLORS_DEFINITIONS_H
+
+#define RESET "\x1B[0m"
+#define RED "\x1B[31m"
+#define BLUE "\x1B[34m"
+#define PURPLE "\x1B[35m"
+#define CYAN "\x1B[36m"
+#define GREEN "\x1B[32m"
+
+#endif
