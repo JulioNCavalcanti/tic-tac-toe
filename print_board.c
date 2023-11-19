b154038e650595e9fcf91c986509098db9b05e67
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 08:47:35 -0300

    Adding colors

--- print_board.c
+++ print_board.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "print_header.h"
 #include "get_board.h"
+#include "colors.h"
 
 void print_board()
 {
@@ -12,7 +13,27 @@ void print_board()
     {
         for (int j = 0; j < 76; j++)
         {
-            printf("%c", board[i][j]);
+
+            if ((i >= 5 && i <= 11) && (board[i][j] == 'A' || board[i][j] == 'B' || board[i][j] == 'C' || board[i][j] == '1' || board[i][j] == '2' || board[i][j] == '3'))
+            {
+                printf("%s%c%s", BLUE, board[i][j], RESET);
+            }
+            else if (board[i][j] == 'X')
+            {
+                printf("%s%c%s", CYAN, board[i][j], RESET);
+            }
+            else if (board[i][j] == 'O')
+            {
+                printf("%s%c%s", PURPLE, board[i][j], RESET);
+            }
+            else if (board[i][j] == 'T' || board[i][j] == 'I' || board[i][j] == 'E')
+            {
+                printf("%s%c%s", GREEN, board[i][j], RESET);
+            }
+            else
+            {
+                printf("%c", board[i][j]);
+            }
         }
         printf("\n");
     }
