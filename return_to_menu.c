eeb0c8ae89c62c732f40a22d4afd653040bb226e
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 09:27:30 -0300

    Adding colors

--- return_to_menu.c
+++ return_to_menu.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "clear_buffer.h"
+#include "colors.h"
 
 int return_to_menu()
 {
     printf("\n");
-    printf("Press Enter to return to the menu... ");
+    printf("%sPress Enter to return to the menu... %s", BLUE, RESET);
     clear_buffer();
 
     return 1;
