8eac37160625c29d9581329a7dd3fc6954d93546
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 14:22:42 -0300

    Adding colors

--- prints_the_winner_of_the_game_or_if_the_game_was_a_tie.c
+++ prints_the_winner_of_the_game_or_if_the_game_was_a_tie.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include "get_board.h"
 #include "print_header.h"
+#include "colors.h"
 
 void prints_the_winner_of_the_game_or_if_the_game_was_a_tie()
 {
     char(*board)[76] = get_board();
-
     char winner = '\0';
+    const char *color;
 
     if (board[2][30] > board[2][48])
     {
         winner = 'X';
+        color = CYAN;
     }
     else if (board[2][30] < board[2][48])
     {
         winner = 'O';
+        color = PURPLE;
     }
     else
     {
+        color = GREEN;
+
         print_header();
 
         printf("|                                                                          |\n");
         printf("|                              --------------                              |\n");
-        printf("|                             |     Tie!     |                             |\n");
+        printf("|                             |     %sTie!%s     |                             |\n", color, RESET);
         printf("|                              --------------                              |\n");
         printf("|                                                                          |\n");
         printf(" -------------------------------------------------------------------------- \n");
@@ -34,7 +39,7 @@ void prints_the_winner_of_the_game_or_if_the_game_was_a_tie()
 
     printf("|                                                                          |\n");
     printf("|              ----------------------------------------------              |\n");
-    printf("|             |     Player %c is the winner of the game!      |             |\n", winner);
+    printf("|             |     %sPlayer %c is the winner of the game!%s      |             |\n", color, winner, RESET);
     printf("|              ----------------------------------------------              |\n");
     printf("|                                                                          |\n");
     printf(" -------------------------------------------------------------------------- \n");
