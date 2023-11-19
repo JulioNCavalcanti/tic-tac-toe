09aa2ba0fddf8eabb8d9fc1a999842cf0d55dd51
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-19 09:13:14 -0300

    Adding colors

--- continue_the_game.c
+++ continue_the_game.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "clear_buffer.h"
+#include "colors.h"
 
 void continue_the_game()
 {
     printf("\n");
-    printf("Press Enter to continue the game... ");
+    printf("%sPress Enter to continue the game... %s", BLUE, RESET);
     clear_buffer();
 }
