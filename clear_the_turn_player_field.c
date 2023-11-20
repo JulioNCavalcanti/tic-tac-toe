c81d1976a768f500637f58001cb91b279b75eaf1
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-20 17:57:35 -0300

    Adding the function that clears the current player's field

--- clear_the_turn_player_field.c
+++ clear_the_turn_player_field.c
@@ -1,8 +1,12 @@
-#include "get_board.h"
+/*
+This function clears the current player's field.
+*/
+
+#include "get_the_board.h"
 
 void clear_the_turn_player_field()
 {
-    char(*board)[76] = get_board();
+    char(*board)[76] = get_the_board();
 
     board[16][15] = ' ';
 }
