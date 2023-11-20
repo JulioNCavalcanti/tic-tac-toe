840f28627c39adb1cc5b7c15079cdbd190f8625f
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-20 11:12:43 -0300

    Added function to display a thank you message before closing the game

---
+++ print_the_thank_you_message.c
@@ -0,0 +1,20 @@
+/*
+This function only serves to display a thank you message before ending the game.
+*/
+
+#include <stdio.h>
+#include "print_the_title.h"
+#include "colors_definitions.h"
+
+void print_the_thank_you_message()
+{
+    print_the_title();
+
+    printf("|                                                                          |\n");
+    printf("|            --------------------------------------------------            |\n");
+    printf("|           |     %sThank you very much for playing my game!%s     |           |\n", PURPLE, RESET);
+    printf("|           |                %sSee you next time.%s                |           |\n", PURPLE, RESET);
+    printf("|            --------------------------------------------------            |\n");
+    printf("|                                                                          |\n");
+    printf(" --------------------------------------------------------------------------  \n");
+}
---
+++ print_the_thank_you_message.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_THANK_YOU_MESSAGE_H
+#define PRINT_THANK_YOU_MESSAGE_H
+
+void print_the_thank_you_message();
+
+#endif
