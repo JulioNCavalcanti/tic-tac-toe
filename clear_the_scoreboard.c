80e3525d42a7bcc281e5a5202677db07b1d85893
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-20 17:59:14 -0300

    Adding the function that clears the score display

--- clear_the_scoreboard.c
+++ clear_the_scoreboard.c
@@ -1,8 +1,12 @@
-#include "get_board.h"
+/*
+This function clears the score display.
+*/
+
+#include "get_the_board.h"
 
 void clear_the_scoreboard()
 {
-    char(*board)[76] = get_board();
+    char(*board)[76] = get_the_board();
 
     board[2][30] = '0';
     board[2][40] = '0';
