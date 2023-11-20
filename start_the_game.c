060bef176b04d38a942ea0954acd23260f7fa4d8
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-20 18:04:59 -0300

    Adding the function that starts the game

--- start_the_game.c
+++ start_the_game.c
@@ -1,28 +1,32 @@
+/*
+This function starts the game.
+*/
+
 #include <stdio.h>
-#include "clear_the_scoreboard.h"
-#include "print_board.h"
-#include "clear_buffer.h"
-#include "sets_the_first_player_at_random.h"
-#include "played_capture.h"
+#include "print_the_board.h"
+#include "clear_the_buffer.h"
+#include "print_the_error_message.h"
+#include "update_the_match_display_on_the_board.h"
+#include "select_random_starting_player.h"
+#include "update_the_current_display_of_the_players_on_the_board.h"
+#include "get_player_movement.h"
 #include "check_the_winner_of_the_match.h"
 #include "update_player_scores.h"
+#include "print_match_victory_message.h"
 #include "continue_the_game.h"
 #include "update_the_tie.h"
-#include "clear_the_tic_tac_toe_board.h"
-#include "prints_the_winner_of_the_game_or_if_the_game_was_a_tie.h"
-#include "return_to_menu.h"
-#include "print_menu.h"
-#include "update_the_turn_player_field.h"
-#include "clear_the_turn_player_field.h"
-#include "update_the_current_match_field.h"
-#include "clear_the_current_match_field.h"
-#include "print_match_victory_message.h"
 #include "print_message_about_tie_in_match.h"
-#include "colors.h"
+#include "clear_the_tic_tac_toe_board.h"
+#include "clear_the_match_display.h"
+#include "clear_the_turn_player_field.h"
+#include "prints_the_winner_of_the_game_or_if_the_game_was_a_tie.h"
+#include "clear_the_scoreboard.h"
+#include "back_to_menu.h"
+#include "colors_definitions.h"
 
 void start_the_game()
 {
-    print_board();
+    print_the_board();
 
     char player_icon = '\0';
     int current_match = 1;
@@ -33,16 +37,11 @@ void start_the_game()
         printf("\n");
         printf("%sEnter the number of matches (1-9): %s", BLUE, RESET);
         scanf("%d", &matches);
-        clear_buffer();
+        clear_the_buffer();
 
         if (matches < 1 || matches > 9)
         {
-            printf("\n");
-            printf("%s --------------------------------------------------- %s\n", RED, RESET);
-            printf("%s| This option does not exist! Enter a valid option. |%s\n", RED, RESET);
-            printf("%s --------------------------------------------------- %s\n", RED, RESET);
-            continue_the_game();
-            print_board();
+            print_the_error_message(4);
             continue;
         }
         break;
@@ -50,8 +49,8 @@ void start_the_game()
 
     while (current_match <= matches)
     {
-        update_the_current_match_field(current_match, matches);
-        int current_player = sets_the_first_player_at_random();
+        update_the_match_display_on_the_board(current_match, matches);
+        int current_player = select_random_starting_player();
         int number_of_move = 1;
 
         while (1)
@@ -68,14 +67,14 @@ void start_the_game()
                     player_icon = 'O';
                 }
 
-                update_the_turn_player_field(player_icon);
-                played_capture(player_icon);
+                update_the_current_display_of_the_players_on_the_board(player_icon);
+                get_player_movement(player_icon);
 
                 number_of_move++;
                 if (number_of_move >= 3 && (check_the_winner_of_the_match(player_icon)))
                 {
                     update_player_scores(player_icon);
-                    print_board();
+                    print_the_board();
                     print_match_victory_message(player_icon);
                     continue_the_game();
                     break;
@@ -86,7 +85,7 @@ void start_the_game()
             else
             {
                 update_the_tie();
-                print_board();
+                print_the_board();
                 print_message_about_tie_in_match();
                 continue_the_game();
                 break;
@@ -95,15 +94,11 @@ void start_the_game()
 
         current_match++;
         clear_the_tic_tac_toe_board();
-        clear_the_current_match_field();
+        clear_the_match_display();
         clear_the_turn_player_field();
     }
 
     prints_the_winner_of_the_game_or_if_the_game_was_a_tie();
     clear_the_scoreboard();
-
-    if (return_to_menu())
-    {
-        print_menu();
-    }
+    back_to_menu();
 }
