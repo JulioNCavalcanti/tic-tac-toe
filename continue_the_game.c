d2c1d2703237151fdaf8659248001cf2569a09cc
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-20 11:22:17 -0300

    Adding the function to continue the game when the Enter key is pressed

--- continue_the_game.c
+++ continue_the_game.c
@@ -1,10 +1,15 @@
+/*
+This function is used to wait for the Enter key to be pressed 
+and only then will the game continue running.
+*/
+
 #include <stdio.h>
-#include "clear_buffer.h"
-#include "colors.h"
+#include "clear_the_buffer.h"
+#include "colors_definitions.h"
 
 void continue_the_game()
 {
     printf("\n");
     printf("%sPress Enter to continue the game... %s", BLUE, RESET);
-    clear_buffer();
+    clear_the_buffer();
 }
